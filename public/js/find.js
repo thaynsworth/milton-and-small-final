e97467925777841fd42b7c2a4ff34a7be1572701
thaynsworth <thomas.haynsworth@gmail.com>
2016-05-02 22:28:41 -0400

    added basic mapbox map

---
+++ public/js/find.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhheW5zd29ydGgiLCJhIjoiWThsS0FVbyJ9.Ra30ahPRIVNTX9FBGK-hLg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/thaynsworth/cinqrm4f7006sc3mb1aeunaca',
+    center: [-122.399482, 37.792598], // starting position
+    zoom: 9.43 // starting zoom
+});
